c7390fdfb785ab8035337e64220059bd8e914269
leila <leila.m2006131@gmail.com>
2025-03-27 02:10:17 +0330

    add Main

--- src/db/Database.java
+++ src/db/Database.java
@@ -3,17 +3,17 @@ package db;
 import java.util.ArrayList;
 
 public class Database {
-    private ArrayList<Entity> entities = new ArrayList<>();
+    private static ArrayList<Entity> entities = new ArrayList<>();
 
     public Database() {
-        throw new UnsupportedOperationException("این کلاس قابل نمونه سازی نیست!");
+
     }
 
-    public void add(Entity e) {
+    public static void add(Entity e) {
         entities.add(e);
     }
 
-    public Entity get(int id) throws EntityNotFoundException {
+    public static Entity get(int id) throws EntityNotFoundException {
         for (Entity e : entities) {
             if (e.id == id) {
                 return e;
@@ -22,7 +22,7 @@ public class Database {
         throw new EntityNotFoundException();
     }
 
-    public void delete(int id) throws EntityNotFoundException {
+    public static void delete(int id) throws EntityNotFoundException {
         for (Entity e : entities) {
             if (e.id == id) {
                 entities.remove(e);
@@ -32,7 +32,7 @@ public class Database {
         throw new EntityNotFoundException();
     }
 
-    public void update(Entity e) throws EntityNotFoundException {
+    public static void update(Entity e) throws EntityNotFoundException {
         for (int i = 0; i < entities.size(); i++) {
             if (entities.get(i).id == e.id) {
                 entities.set(i, e);
