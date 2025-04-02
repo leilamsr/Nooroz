5418458135f428cde660c1bd2b228927c603ad1e
leila <leila.m2006131@gmail.com>
2025-04-02 21:02:10 +0330

    edit Human

--- src/example/Human.java
+++ src/example/Human.java
@@ -7,17 +7,14 @@ public class Human extends Entity {
     public static int age;
     public static final int HUMAN_ENTITY_CODE = 14;
 
-    public Human(String name) {
+    public Human(String name, int age) {
         this.name = name;
-    }
-
-    public Human(int age) {
         this.age = age;
     }
 
     @Override
     public Human copy() {
-        Human copyHuman = new Human(name);
+        Human copyHuman = new Human(name, age);
         copyHuman.id = id;
         copyHuman.age = age;
 
