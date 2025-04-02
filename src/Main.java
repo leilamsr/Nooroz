47b41e820602a4b3bc617eb48baac8136511d964
leila <leila.m2006131@gmail.com>
2025-04-02 21:24:25 +0330

    edit Main

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,11 @@ public class Main {
     public static void main(String[] args) throws InvalidEntityException {
         Database.registerValidator(Human.HUMAN_ENTITY_CODE, new HumanValidator());
 
-        Human ali = new Human("Ali", -10);
-        Database.add(ali);
+        Human ali = new Human("Ali", 10);
+        try {
+            Database.add(ali);
+        } catch (InvalidEntityException e) {
+            System.err.println("خطا در ثبت اطلاعات: " + e.getMessage());
+        }
     }
 }
