cf12b065ea8bff569eadc8bbdab5fa538429abfa
leila <leila.m2006131@gmail.com>
2025-04-04 00:03:53 +0330

    add Document

---
+++ src/example/Document.java
@@ -0,0 +1,56 @@
+package example;
+
+import db.Entity;
+import db.Trackable;
+
+import java.util.Date;
+
+public class Document extends Entity implements Trackable {
+    private Date CreationDate;
+    private Date LastModificationDate;
+    private String content;
+
+    public Document(String content) {
+        this.content = content;
+        this.CreationDate = new Date();
+        this.LastModificationDate = new Date();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public void setCreationDate(Date date) {
+        this.CreationDate = date;
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return CreationDate;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date) {
+        this.LastModificationDate = date;
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return LastModificationDate;
+    }
+
+    @Override
+    public Entity copy() {
+        Document copyDocument = new Document(this.content);
+        copyDocument.id = this.id;
+        copyDocument.CreationDate = this.CreationDate;
+        copyDocument.LastModificationDate = this.LastModificationDate;
+        return copyDocument;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return 0;
+    }
+}
