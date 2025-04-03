022425126fd933cc1709416f86ea0e60460c7c0e
leila <leila.m2006131@gmail.com>
2025-04-03 23:16:07 +0330

    add Trackable

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws InvalidEntityException {
         Database.registerValidator(Human.HUMAN_ENTITY_CODE, new HumanValidator());
 
-        Human ali = new Human("Ali", 10);
+        Human ali = new Human("Ali", -10);
         try {
             Database.add(ali);
         } catch (InvalidEntityException e) {
