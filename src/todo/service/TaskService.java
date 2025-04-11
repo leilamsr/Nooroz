b790e442b2246677446102bb2ee260faf1463a30
leila <leila.m2006131@gmail.com>
2025-04-11 22:24:57 +0330

    add method taskservice

--- src/todo/service/TaskService.java
+++ src/todo/service/TaskService.java
@@ -1,4 +1,49 @@
 package todo.service;
 
+import todo.entity.Task;
+import exception.InvalidEntityException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TaskService {
+
+    private static Map<Integer, Task> taskData = new HashMap<>();
+
+    public static void setAsCompleted(int taskId) throws InvalidEntityException {
+        Task task = taskData.get(taskId);
+
+        if (task == null) {
+            throw new InvalidEntityException("تسکی با این شناسه یافت نشد.");
+        }
+
+        task.setStatus(Task.Status.Completed);
+        taskData.put(taskId, task);
+    }
+
+    public static void setAsInProgress(int taskId) throws InvalidEntityException {
+        Task task = taskData.get(taskId);
+
+        if (task == null) {
+            throw new InvalidEntityException("تسکی با این شناسه یافت نشد!");
+        }
+
+        task.setStatus(Task.Status.InProgress);
+        taskData.put(taskId, task);
+    }
+
+    public static void setAsNotStarted(int taskId) throws InvalidEntityException {
+        Task task = taskData.get(taskId);
+
+        if (task == null) {
+            throw new InvalidEntityException("تسکی با این شناسه یافت نشد!");
+        }
+
+        task.setStatus(Task.Status.NotStarted);
+        taskData.put(taskId, task);
+    }
+
+    public static void addTask(int taskId, Task task) {
+        taskData.put(taskId, task);
+    }
 }
