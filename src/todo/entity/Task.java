42329c3af685873daa13e5ca6465139e3e6e39ce
leila <leila.m2006131@gmail.com>
2025-04-11 21:38:18 +0330

    add method task

--- src/todo/entity/Task.java
+++ src/todo/entity/Task.java
@@ -1,5 +1,93 @@
 package todo.entity;
 
-public class Task {
+import db.Entity;
+import db.Trackable;
+
+import java.util.Date;
+
+public class Task extends Entity implements Trackable {
+
+    private String title;
+    private String description;
+    private Date dueDate;
+    private Status status;
+
+    private Date creationDate;
+    private Date lastModificationDate;
+
+    public Task(String title, String description, Date dueDate, Status status) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public void setCreationDate(Date date) {
+        this.creationDate = date;
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return this.creationDate;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date) {
+        this.lastModificationDate = date;
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return this.lastModificationDate;
+    }
+
+    @Override
+    public Entity copy() {
+        return new Task(this.title, this.description, this.dueDate, this.status);
+    }
+
+    @Override
+    public int getEntityCode() {
+        return this.id;
+    }
+
+    public enum Status {
+        NotStarted,
+        InProgress,
+        Completed
+    }
 }
 
