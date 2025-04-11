17e2d0c5f128f36ecb7a13d753fbeb3f2099675e
leila <leila.m2006131@gmail.com>
2025-04-11 21:58:15 +0330

    add method taskvalidator

--- src/todo/validator/TaskValidator.java
+++ src/todo/validator/TaskValidator.java
@@ -1,4 +1,22 @@
 package todo.validator;
 
-public class TaskValidator {
+import db.Validator;
+import db.Entity;
+import todo.entity.Task;
+import exception.InvalidEntityException;
+
+public class TaskValidator implements Validator {
+
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        if (!(entity instanceof Task)) {
+            throw new InvalidEntityException("Entity must be an instance of Task.");
+        }
+
+        Task task = (Task) entity;
+
+        if (task.getTitle() == null || task.getTitle().trim().isEmpty()) {
+            throw new InvalidEntityException("Task title cannot be null or empty.");
+        }
+    }
 }
