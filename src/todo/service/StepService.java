a41297f33be116607b31e425e67d6cdffa75174a
leila <leila.m2006131@gmail.com>
2025-04-11 22:35:24 +0330

    add method stepservice

--- src/todo/service/StepService.java
+++ src/todo/service/StepService.java
@@ -1,4 +1,43 @@
 package todo.service;
 
+import todo.entity.Step;
+import exception.InvalidEntityException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class StepService {
+
+    private static Map<Integer, Step> stepData= new HashMap<>();
+
+    public static void saveStep(int taskRef, String title) {
+        Step step = new Step(title, Step.Status.NotStarted, taskRef);
+        stepData.put(step.hashCode(), step);
+    }
+
+    public static void setStepAsCompleted(int stepId) throws InvalidEntityException {
+        Step step = stepData.get(stepId);
+
+        if (step == null) {
+            throw new InvalidEntityException("گامی با این شناسه یافت نشد!");
+        }
+
+        step.setStatus(Step.Status.Completed);
+        stepData.put(stepId, step);
+    }
+
+    public static void setStepAsNotStarted(int stepId) throws InvalidEntityException {
+        Step step = stepData.get(stepId);
+
+        if (step == null) {
+            throw new InvalidEntityException("گامی با این شناسه یافت نشد!");
+        }
+
+        step.setStatus(Step.Status.NotStarted);
+        stepData.put(stepId, step);
+    }
+
+    public static void addStep(int stepId, Step step) {
+        stepData.put(stepId, step);
+    }
 }
