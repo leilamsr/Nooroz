d240b671a6dcad55853163fa8693bdf1428f7f29
leila <leila.m2006131@gmail.com>
2025-04-11 22:10:22 +0330

    add method stepvalidator

--- src/todo/validator/StepValidator.java
+++ src/todo/validator/StepValidator.java
@@ -1,4 +1,26 @@
 package todo.validator;
 
-public class StepValidator {
+import db.Validator;
+import db.Entity;
+import todo.entity.Step;
+import exception.InvalidEntityException;
+
+public class StepValidator implements Validator {
+
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        if (!(entity instanceof Step)) {
+            throw new InvalidEntityException("موجودیت باید از نوع Step باشد!");
+        }
+
+        Step step = (Step) entity;
+
+        if (step.getTitle() == null || step.getTitle().trim().isEmpty()) {
+            throw new InvalidEntityException("عنوان قدم نمی‌تواند خالی باشد!");
+        }
+
+        if (step.getTaskRef() <= 0) {
+            throw new InvalidEntityException("taskRef باید یک شناسه معتبر باشد!");
+        }
+    }
 }
