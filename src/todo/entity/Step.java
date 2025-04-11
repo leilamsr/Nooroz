41fa64132ead129a4b6e2dafa1d1ce2995b8ab0d
leila <leila.m2006131@gmail.com>
2025-04-11 21:53:52 +0330

    add method step

--- src/todo/entity/Step.java
+++ src/todo/entity/Step.java
@@ -1,4 +1,55 @@
 package todo.entity;
 
-public class Step {
+import db.Entity;
+import jdk.net.SocketFlow;
+
+public class Step extends Entity {
+    private String title;
+    private Status status;
+    private int taskRef;
+
+    public Step(String title, Status status, int taskRef) {
+        this.title = title;
+        this.status = status;
+        this.taskRef = taskRef;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public int getTaskRef() {
+        return taskRef;
+    }
+
+    public void setTaskRef(int taskRef) {
+        this.taskRef = taskRef;
+    }
+
+    @Override
+    public Entity copy() {
+        return new Step(this.title, this.status, this.taskRef);
+    }
+
+    @Override
+    public int getEntityCode() {
+        return this.id;
+    }
+
+    public enum Status {
+        NotStarted,
+        Completed
+    }
 }
